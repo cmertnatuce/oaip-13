7ed08232b6e6fcceea5808e8dfc42c531093bd21
cmertnatuce <157032600+cmertnatuce@users.noreply.github.com>
2024-04-10 12:18:42 +0300

    Добавьте файлы проекта.

---
+++ oaip 13/oaip 13.cpp
@@ -0,0 +1,96 @@
+﻿/*Создать стек для символов.Максимальный размер стека вводится с экрана.
+Создать функции для ввода, вывода и определения размера стека.
+Вводить символы с экрана в стек.
+В случае совпадения вводимого символа с вершиной стека  вывести размер стека.*/
+
+#include <iostream>
+#include <windows.h>
+#include <locale>
+
+using namespace std;
+
+class Stack {
+private:
+    char* arr;
+    int top;
+    int maxSize;
+public:
+    Stack(int size) {
+        maxSize = size;
+        arr = new char[maxSize];
+        top = -1;
+    }
+
+    ~Stack() {
+        delete[] arr;
+    }
+
+    void push(char c) {
+        if (top == maxSize - 1) {
+            cout << "Стек переполнен\n";
+            return;
+        }
+        arr[++top] = c;
+    }
+
+    char pop() {
+        if (top == -1) {
+            cout << "Стек пуст\n";
+            return '\0';
+        }
+        return arr[top--];
+    }
+
+    int getSize() {
+        return top + 1;
+    }
+
+    char peek() {
+        if (top == -1) {
+            cout << "Стек пуст\n";
+            return '\0';
+        }
+        return arr[top];
+    }
+};
+
+int main() {
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    setlocale(LC_ALL, "Rus");
+
+    int maxSize;
+    cout << "Введите максимальный размер стека: ";
+    cin >> maxSize;
+
+    Stack stack(maxSize);
+
+    int choice;
+    char symbol;
+    while (true) {
+        cout << "\nМеню:\n";
+        cout << "1. Ввести символ в стек\n";
+        cout << "2. Вывести размер стека\n";
+        cout << "3. Выйти\n";
+        cout << "Выберите действие: ";
+        cin >> choice;
+
+        switch (choice) {
+        case 1:
+            cout << "Введите символ: ";
+            cin >> symbol;
+            stack.push(symbol);
+            break;
+        case 2:
+            cout << "Размер стека: " << stack.getSize() << endl;
+            break;
+        case 3:
+            cout << "Выход из программы\n";
+            return 0;
+        default:
+            cout << "Некорректный ввод\n";
+        }
+    }
+
+    return 0;
+}
